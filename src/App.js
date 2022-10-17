38c4591e3f3c9963578ce87787840da514ce393e
jam-jam200 <jessieoyaks4j@gmail.com>
2022-10-17 21:15:39 +0100

    url shortener

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import BackgroundAnimation from "./components/BackgroundAnimation";
 import InputShortener from "./components/InputShortener";
 import Result from "./components/Result";
 import React, { useState } from "react";
+import { Helmet } from "react-helmet";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   return (
     <div className="App appContainer">
-      <InputShortener setInputValue={setInputValue}/>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>URLShortz</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="shortening url links" />
+      </Helmet>
+      <InputShortener setInputValue={setInputValue} />
       <BackgroundAnimation />
-      <Result inputValue={inputValue}/>
+      <Result inputValue={inputValue} />
     </div>
   );
 }
